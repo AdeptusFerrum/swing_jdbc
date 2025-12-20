5aa845a4412501dc0ab24097ff36b45bf7400709
AdeptusFerrum <vitalik.kleymenov.91@mail.ru>
2025-12-20 13:40:36 +0300

    ┬─┬ノ( º _ ºノ)

--- devsecops-reporter/send_telegram.sh
+++ devsecops-reporter/send_telegram.sh
@@ -5,6 +5,6 @@ CHAT_ID="$CHAT_ID"
 
 TEXT="DevSecOps Report:%0A$(jq -r '.[] | "\(.tool): \(.status)"' summary.json)"
 
-curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
+curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
   -d chat_id="$CHAT_ID" \
   -d text="$TEXT"
